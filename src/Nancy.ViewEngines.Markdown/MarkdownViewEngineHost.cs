67898337fc149ddbf80a58eef4e4340bfd2a6bcf
Jonathan Channon <jonathan.channon@gmail.com>
2013-01-31 14:37:22 +0000

    Refactored slightly

--- src/Nancy.ViewEngines.Markdown/MarkdownViewEngineHost.cs
+++ src/Nancy.ViewEngines.Markdown/MarkdownViewEngineHost.cs
@@ -13,7 +13,7 @@ namespace Nancy.ViewEngines.Markdown
         private readonly IRenderContext renderContext;
         private readonly MarkdownSharp.Markdown parser;
         private static readonly IEnumerable<string> validExtensions = new[] { "md", "markdown" };
-        private static readonly Regex ParagraphSubstitution = new Regex("<p>(@[^<]*)</p>", RegexOptions.Compiled);
+        private static readonly Regex ParagraphSubstitution = new Regex("<p>(@[^<]*)</p>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
 
         public MarkdownViewEngineHost(IViewEngineHost viewEngineHost, IRenderContext renderContext)
@@ -42,43 +42,38 @@ namespace Nancy.ViewEngines.Markdown
 
             var masterpartialContent = viewLocationResult.Contents.Invoke().ReadToEnd();
 
-            if (!ValidExtensions.Any(x => x.Equals(viewLocationResult.Extension, StringComparison.OrdinalIgnoreCase)))
+            if (!validExtensions.Any(x => x.Equals(viewLocationResult.Extension, StringComparison.OrdinalIgnoreCase)))
             {
                 return masterpartialContent;
             }
 
-            string html;
-
-            if (viewLocationResult.Name == "master")
+            if (viewLocationResult.Name.ToLower() == "master")
             {
-                string header = masterpartialContent.Substring(masterpartialContent.IndexOf("<!DOCTYPE html>", StringComparison.Ordinal),
-                                                                masterpartialContent.IndexOf("<body>", StringComparison.Ordinal) + 6);
+                string header = masterpartialContent.Substring(masterpartialContent.IndexOf("<!DOCTYPE html>", StringComparison.OrdinalIgnoreCase),
+                                                                masterpartialContent.IndexOf("<body>", StringComparison.OrdinalIgnoreCase) + 6);
 
                 string toConvert =
                     masterpartialContent.Substring(
-                        masterpartialContent.IndexOf("<body>", StringComparison.Ordinal) + 6,
-                        (masterpartialContent.IndexOf("</body>", StringComparison.Ordinal) - 7) -
-                        (masterpartialContent.IndexOf("<body>", StringComparison.Ordinal)));
+                        masterpartialContent.IndexOf("<body>", StringComparison.OrdinalIgnoreCase) + 6,
+                        (masterpartialContent.IndexOf("</body>", StringComparison.OrdinalIgnoreCase) - 7) -
+                        (masterpartialContent.IndexOf("<body>", StringComparison.OrdinalIgnoreCase)));
 
-                string footer = masterpartialContent.Substring(masterpartialContent.IndexOf("</body>", StringComparison.Ordinal));
+                string footer = masterpartialContent.Substring(masterpartialContent.IndexOf("</body>", StringComparison.OrdinalIgnoreCase));
 
-                    html = parser.Transform(toConvert);
+                string html = parser.Transform(toConvert);
 
-                    var serverHtml = ParagraphSubstitution.Replace(html, "$1");
+                var serverHtml = ParagraphSubstitution.Replace(html, "$1");
 
-                    return string.Concat(header, serverHtml, footer);
-                }
-                else
-                {
-                    var parser = new MarkdownSharp.Markdown();
-                    html = parser.Transform(masterpartialContent);
-                }
-
-                return html;
+                return string.Concat(header, serverHtml, footer);
             }
-            
-            return masterpartialContent;
+            else
+            {
+                var parser = new MarkdownSharp.Markdown();
+                return  parser.Transform(masterpartialContent);
+            }
+
         }
+
 
         public string GetUriString(string name, params string[] parameters)
         {
